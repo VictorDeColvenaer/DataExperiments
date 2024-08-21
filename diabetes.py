2a1d69b825317636273c4dae57b2ee2fa36034c3
Victor De Colvenaer <victordecolvenaer@gmail.com>
2024-08-21 20:08:43 +0200

    commit

--- diabetes.py
+++ diabetes.py
@@ -13,6 +13,7 @@ summary = str(df.describe)
 missing_values = str(df.isnull().sum())
 data_types = str(df.dtypes)
 info = str(df.info())
+zero_values = str((df == 0).sum(axis=0))
 
 #print exploratory information in txt file
 output = open('output.txt','w')
@@ -22,13 +23,14 @@ print("summary\n" + summary, file = output)
 print("missing value\n" + missing_values, file = output)
 print("data types\n" + data_types, file = output)
 print("info\n" + info, file = output)
+print("zerovalues in column\n" + zero_values, file = output)
 
 
 #create PDF file to save figures from following visualisations
 pdf_filename = 'output_plots.pdf'
 
 #create some exploratory data visualisations
-numerical_columns = ['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']
+numerical_columns = ['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age','Outcome']
 categorical_columns = ['Outcome']
 correlation_matrix = df[numerical_columns].corr()
 
@@ -36,7 +38,7 @@ correlation_matrix = df[numerical_columns].corr()
 with PdfPages(pdf_filename) as pdf_pages:
     for col in numerical_columns:
         plt.figure()
-        sns.violinplot(df[col])
+        sns.boxplot(df[col])
         plt.title(col)
         pdf_pages.savefig()
         plt.close()
@@ -48,32 +50,16 @@ with PdfPages(pdf_filename) as pdf_pages:
         pdf_pages.savefig()
         plt.close()
 
-    #create scatterplots with imdb_score
-    figure, subplot = plt.subplots(2,2)
-    x_labels = ['Pregnancies','Glucose','BloodPressure','SkinThickness']
-    for i,data in enumerate(x_labels):
-        sns.scatterplot(x=df[data], y=df['Outcome'],ax = subplot[i//2,i%2])
-        subplot[i//2,i%2].set_xlabel({data})
-        subplot[i//2,i%2].set_ylabel('Outcome')
-        subplot[i//2,i%2].set_title(f'Scatterplot of {data} vs IMDB Score')
-
-    pdf_pages.savefig()
-    plt.close()
-
-    figure, subplot = plt.subplots(2,2)
-    x_lables = ['Insulin','BMI','DiabetesPedigreeFunction','Age']
-    for i,data in enumerate(x_labels):
-        sns.scatterplot(x=df[data], y=df['Outcome'],ax = subplot[i//2,i%2])
-        subplot[i//2,i%2].set_xlabel({data})
-        subplot[i//2,i%2].set_ylabel('Outcome')
-        subplot[i//2,i%2].set_title(f'Scatterplot of {data} vs IMDB Score')
-
-    pdf_pages.savefig()
-    plt.close()
-
     # Visualize correlation matrix
     plt.figure(figsize=(10, 8))
     sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
     plt.title('Correlation Matrix')
+    pdf_pages.savefig()
+    plt.close()
+
+    #visualize pairplot
+    plt.figure()
+    sns.pairplot(df, hue = "Outcome",kind = 'kde')
+    plt.title('pairplot')
     pdf_pages.savefig()
     plt.close()
